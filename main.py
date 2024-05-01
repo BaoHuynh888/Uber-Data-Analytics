9d61514401a02b77bec8412b8fdad5670e9d1ff7
Bao Huynh <baohuynh888@gmail.com>
2024-05-01 15:57:45 -0700

    Adding code to transforming data

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import pandas as pd
-df = pd.read_csv("data/taxi_data.csv")
+df = pd.read_csv("data/uber_data.csv")
 df.head()
+
+df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+datetime_dim = df[['tpep_pickup_datetime', 'tpep_dropoff_datetime']].drop_duplicates().reset_index(drop=True)
